792c8893f3f76170018c01d0c82bea9f3f9d7889
thenoobchad <noobchaddy@gmail.com>
2025-12-28 12:09:50 +0100

    clean

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@
 
 export default function Home() {
   return (
-    <main className="bg-zinc-300 text-zinc-800">
+    <main className="bg-zinc-300 text-zinc-800 py-8">
       hi there, lets begin
+
+      <button className="bg-white text-zinc-900 px-6 py-2  border-4 border-zinc-900 font-semibold text-sm shadow-[6px_6px_0px_#000] w-fit active:scale-95">SUBSCRIBE</button>
    </main>
   );
 }
